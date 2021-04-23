97da89017a5d72abc7df93eefb2d160f5db45083
ammych <77796939+ammych@users.noreply.github.com>
2021-04-23 23:49:50 +0530

    Add files via upload

---
+++ projects/storefront/src/app/modules/shared/components/address-card/address-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, HostBinding, Input, TemplateRef } from '@angular/core';
+import { AddressData } from '../../../../interfaces/address';
+
+@Component({
+    selector: 'app-address-card',
+    templateUrl: './address-card.component.html',
+})
+export class AddressCardComponent {
+    @Input() address: AddressData;
+
+    @Input() label: string;
+
+    @Input() featured = false;
+
+    @Input() footer: TemplateRef<any>;
+
+    @Input() loading = false;
+
+    @HostBinding('class.card') classCard = true;
+
+    @HostBinding('class.card--loading') get classCardLoading(): boolean { return this.loading; }
+
+    @HostBinding('class.address-card') classAddressCard = true;
+
+    @HostBinding('class.address-card--featured') get classAddressCardFeatured(): boolean { return this.featured; }
+
+    constructor() { }
+}
